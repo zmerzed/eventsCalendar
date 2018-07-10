daa597c5d4d7d32ceb2cc6e00a5a1ae54fbbbfa1
DESKTOP-1LMTG2V\Win PC <zmerzed@gmail.com>
2018-07-10 20:44:18 +0800

    updates migration

---
+++ database/migrations/2018_07_10_124047_create_events_detail_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventsDetail', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('eventsMaster');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventsDetail');
+    }
+}
