d226a423a012f89f33646eee33c08292698ee096
remz <zmerzed@gmail.com>
2018-07-13 11:04:51 +0800

    updates

--- resources/views/calendar2.blade.php
+++ resources/views/calendar2.blade.php
@@ -60,7 +60,7 @@
             <div class="col-lg-12" style="margin-top:70px">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Calendar
+                        <h4><strong>Calendar</strong></h4>
                     </div>
                     <div class="panel-body">
                         <div class="col-lg-4 col-md-4">
@@ -81,34 +81,34 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <div class="checkbox pull-left" ng-repeat="day in event.weekDays">
+                                    <div class="checkbox pull-left" style="margin:0; margin-right: 7px" ng-repeat="day in event.weekDays">
                                         <label><input type="checkbox" ng-model="day.isChecked">@{{ day.label }}</label>
                                     </div>
-
                                 </div>
+                            </div>
+                            <div class="row">
                                 <div class="form-group">
                                     <button type="button" class="btn btn-primary" ng-click="save()">Save</button>
-
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-8 col-md-8">
 
                             <div class="form-group">
-                                <h4>@{{ event.query.fromMonthYear }}</h4>
+                                <h3><strong>@{{ event.query.fromMonthYear }}</strong></h3>
 
                                 <table class="table">
                                     <tbody>
                                     <tr ng-repeat="day in event.fromDays">
                                         <td ng-class="{success: isMatch(day)}">@{{ day.day }} @{{ day.dayName }}</td>
-                                        <td></td>
+                                        <td ng-class="{success: isMatch(day)}">@{{ event.data.name }}</td>
                                     </tr>
                                     </tbody>
                                 </table>
                             </div>
-                            <div class="form-group">
-                                <h4>@{{ event.query.toMonthYear }}</h4>
 
+                            <div class="form-group" ng-show="event.query.fromMonthYear != event.query.toMonthYear">
+                                <h3><strong>@{{ event.query.toMonthYear }}</strong></h3>
                                 <table class="table">
                                     <tbody>
                                     <tr ng-repeat="day in event.toDays">
