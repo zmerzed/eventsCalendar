138fb30de3f48d92f508eea4e70651c52ced322b
remz <zmerzed@gmail.com>
2018-07-13 09:31:20 +0800

    updates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,19 +11,31 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-    }
+//    public function boot()
+//    {
+//        Schema::defaultStringLength(191);
+//    }
 
-    
+    public function boot(UrlGenerator $url)
+    {
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+    }
     /**
      * Register any application services.
      *
      * @return void
      */
+//    public function register()
+//    {
+//        //
+//    }
+
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
